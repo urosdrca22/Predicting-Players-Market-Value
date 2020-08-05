db98045ff154f6a8981326719dd7e3d88aa3f0f2
Uros Drca <urosdrca22@gmail.com>
2020-08-05 20:28:10 +0200

    A web crawler I wrote in order to scrape the player data https://sofifa.com

--- spider.py
+++ spider.py
@@ -3,7 +3,7 @@ import scrapy
 class PlayerValue(scrapy.Spider):
 	name = "players"
 	start_urls = [
-	'https://sofifa.com'
+	'https://sofifa.com/?col=vl&sort=desc'
 	]
 
 	def parse(self, response):
@@ -19,13 +19,13 @@ class PlayerValue(scrapy.Spider):
 
 		try:
 			next_page_partial_url = response.xpath('//div[@class="pagination"]/a/@href')[1].extract()  
-			next_page = 'https://sofifa.com' + next_page_partial_url
+			next_page = 'https://sofifa.com/?col=vl&sort=desc' + '&' + next_page_partial_url
 			if next_page is not None:
 				next_page = response.urljoin(next_page)
 				yield scrapy.Request(next_page, callback=self.parse)	
 		except IndexError:
 			next_page_partial_url = response.xpath('//div[@class="pagination"]/a/@href').extract_first()
-			next_page = 'https://sofifa.com' + next_page_partial_url
+			next_page = 'https://sofifa.com/?col=vl&sort=desc' + '&' + next_page_partial_url
 			if next_page is not None:
 				next_page = response.urljoin(next_page)
 				yield scrapy.Request(next_page, callback=self.parse)
